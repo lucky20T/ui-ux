5b5ae4f5378161492f1e7182069757469bbbe324
Your Name <you@example.com>
2025-04-15 21:45:38 +0530

    final

--- ux/src/App.jsx
+++ ux/src/App.jsx
@@ -1,9 +1,10 @@
-
+import Header from "./component/Header.jsx"
 function App() {
   
 
   return (
     <>
+    <Header/>
 
     </>
   )
---
+++ ux/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div >
+      <h1 className="header-title">let's go</h1> 
+      <p className="text-lg text-red-400">Welcome to our website!</p> 
+    </div>
+  )
+}
+
+export default Header
